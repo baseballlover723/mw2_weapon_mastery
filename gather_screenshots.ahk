17c391a6674df3a8d57ef61e0d7466225bb2c427
Philip Ross <dev@baseballlover723.com>
2023-03-24 13:55:08 -0700

    clean up

--- gather_screenshots.ahk
+++ gather_screenshots.ahk
@@ -1,19 +1,12 @@
 ﻿#Requires AutoHotkey v2.0
-;#Include "AutoHotkey-JSON/Jxon.ahk"
-;#Include native-test/Native.ahk
 #Include "Gdip_All.ahk"
-;#Include "screenshot.ahk"
-
-
-SleepTime := 100
-NewScreenTime := 400
-;ScreenshotTime := 2000
-;ScreenshotTime := 500
-ScreenshotTime := 100
 
 RunDir := "./runs"
 
-
+SleepTime := 100
+NewScreenTime := 400
+ScreenshotTime := 100
+Modes := Map("full", 1, "gold", "678|485|92|25", "plat", "1328|485|92|25", "poly", "678|1049|92|25", "orion", "1328|1049|92|25")
 x := 0
 y := 0
 
@@ -134,7 +127,6 @@ CreateRunDir() {
     DirDelete RunDir, 1
   }
   DirCreate RunDir
-  DirCreate RunDir . "/flat"
 }
 
 CreateWeaponClassDir(WeaponClass) {
@@ -149,14 +141,11 @@ CreateWeaponDir(WeaponClass, WeaponName) {
 
 GoToChallenges() {
   ; From home navagate to challenges
-  MySend("{F1}", "{Right}", "{Right}", "{Right}", "{Down}")
-;  Send "{F1}"
-;  Sleep NewScreenTime
-;  MySend("{Right}", "{Right}", "{Right}", "{Down}")
-
-  ; Select weapon challenges from menu
-  Send "{Space}"
+  Send "{F1}"
   Sleep NewScreenTime
+
+  MySend("{Right}", "{Right}", "{Right}", "{Down}", "{Space}")
+  ; Select weapon challenges from menu
   Send "{Space}"
   Sleep NewScreenTime
 }
@@ -166,45 +155,30 @@ TakeScreenshot(WeaponClassName, gun) {
   CreateWeaponDir(WeaponClassName, gun)
   ; Select Weapon Mastery
   Send "{Space}"
-;  Sleep NewScreenTime
   Sleep SleepTime
   Send "2"
   Sleep ScreenshotTime
 
-  SaveScreenshot(WeaponClassName, gun, "full")
+;  SaveScreenshot(WeaponClassName, gun, "full")
   SaveScreenshot(WeaponClassName, gun, "gold")
   SaveScreenshot(WeaponClassName, gun, "plat")
   SaveScreenshot(WeaponClassName, gun, "poly")
   SaveScreenshot(WeaponClassName, gun, "orion")
 
-;  Sleep SleepTime
   Send "{Esc}"
   Sleep SleepTime
-;  Sleep NewScreenTime
   Send "{Esc}"
   Sleep NewScreenTime
 }
 
-;Modes := Map("full", 1, "gold", "1300|1050|50|25", "plat", "1308|485|50|25", "poly", "650|1050|50|25", "orion", "1308|1050|50|25")
-;Modes := Map("full", 1, "gold", "680|485|90|25", "plat", "1330|485|90|25", "poly", "680|1050|90|25", "orion", "1330|1050|90|25")
-;Modes := Map("full", 1, "gold", "675|480|100|35", "plat", "1325|480|100|35", "poly", "675|1045|100|35", "orion", "1325|1045|100|35")
-;Modes := Map("full", 1, "gold", "673|480|100|35", "plat", "1323|480|100|35", "poly", "673|1044|100|35", "orion", "1323|1044|100|35")
-
-Modes := Map("full", 1, "gold", "678|485|90|25", "plat", "1328|485|90|25", "poly", "678|1049|90|25", "orion", "1328|1049|90|25")
-;Modes := Map("full", 1, "gold", "678|485|45|25", "plat", "1328|485|45|25", "poly", "678|1049|45|25", "orion", "1328|1049|45|25")
-;Modes := Map("full", 1, "gold", "678|485|40|25", "plat", "1328|485|40|25", "poly", "678|1049|40|25", "orion", "1328|1049|40|25")
-;Modes := Map("full", 1, "gold", "673|485|45|25", "plat", "1323|485|45|25", "poly", "673|1049|45|25", "orion", "1323|1049|45|25")
 
 SaveScreenshot(WeaponClassName, gun, mode) {
   global RunDir
   global Modes
   global oI
   path := RunDir . "/" . WeaponClassName . "/" . gun . "/" . mode . ".png"
-  flatPath := RunDir . "/flat/" . gun . "-" . mode . ".png"
   img:=Gdip_Bitmapfromscreen(Modes[mode])
-;  img:=ResConImg(img, 5)
   Gdip_SaveBitmapToFile(img, path)
-;  Gdip_SaveBitmapToFile(img, flatPath)
   Gdip_DisposeImage(img)
 }
 
@@ -236,11 +210,11 @@ WeaponClass(WeaponClassName, Guns) {
   CreateWeaponClassDir(WeaponClassName)
   for index, gun in Guns {
     NextGun()
-    if (index = 1) { ; DEBUG
-    continue ; DEBUG
-    } ; DEBUG
+;    if (index = 1) { ; DEBUG
+;    continue ; DEBUG
+;    } ; DEBUG
     TakeScreenshot(WeaponClassName, gun)
-    break
+;    break
   }
 }
 
@@ -263,70 +237,11 @@ WinActivate
 
 ptok := Gdip_Startup()
 
-GoToChallenges()
 CreateRunDir()
+GoToChallenges()
 
 WeaponClasses()
 
 MySend("{Esc}", "{Esc}", "{Esc}")
 
 Gdip_Shutdown(ptok)
-
-;TakeScreenshot()
-;NextGun()
-;
-;TakeScreenshot()
-;NextGun()
-;
-;TakeScreenshot()
-;NextGun()
-
-;/*  ResConImg
-; *    By kon
-; *    Updated November 2, 2015
-; *    http://ahkscript.org/boards/viewtopic.php?f=6&t=2505&p=13640#p13640
-; *
-; *  Resize and convert images. png, bmp, jpg, tiff, or gif.
-; *
-; *  Requires Gdip.ahk in your Lib folder or #Included. Gdip.ahk is available at:
-; *      http://www.autohotkey.com/board/topic/29449-gdi-standard-library-145-by-tic/
-; *
-; *  ResConImg( OriginalFile             ;- Path of the file to convert
-; *           , NewWidth                 ;- Pixels (Blank = Original Width)
-; *           , NewHeight                ;- Pixels (Blank = Original Height)
-; *           , NewName                  ;- New file name (Blank = "Resized_" . OriginalFileName)
-; *           , NewExt                   ;- New file extension can be png, bmp, jpg, tiff, or gif (Blank = Original extension)
-; *           , NewDir                   ;- New directory (Blank = Original directory)
-; *           , PreserveAspectRatio      ;- True/false (Blank = true)
-; *           , BitDepth)                ;- 24/32 only applicable to bmp file extension (Blank = 24)
-; */
-;ResConImg(OriginalFile, NewWidth:="", NewHeight:="", NewName:="", NewExt:="", NewDir:="", PreserveAspectRatio:=true, BitDepth:=24) {
-;    SplitPath, OriginalFile, SplitFileName, SplitDir, SplitExtension, SplitNameNoExt, SplitDrive
-;    pBitmapFile := Gdip_CreateBitmapFromFile(OriginalFile)                  ; Get the bitmap of the original file
-;    Width := Gdip_GetImageWidth(pBitmapFile)                                ; Original width
-;    Height := Gdip_GetImageHeight(pBitmapFile)                              ; Original height
-;    NewWidth := NewWidth ? NewWidth : Width
-;    NewHeight := NewHeight ? NewHeight : Height
-;    NewExt := NewExt ? NewExt : SplitExtension
-;    if SubStr(NewExt, 1, 1) != "."                                          ; Add the "." to the extension if required
-;        NewExt := "." NewExt
-;    NewPath := ((NewDir != "") ? NewDir : SplitDir)                         ; NewPath := Directory
-;            . "\" ((NewName != "") ? NewName : "Resized_" SplitNameNoExt)       ; \File name
-;            . NewExt                                                            ; .Extension
-;    if (PreserveAspectRatio) {                                              ; Recalcultate NewWidth/NewHeight if required
-;        if ((r1 := Width / NewWidth) > (r2 := Height / NewHeight))          ; NewWidth/NewHeight will be treated as max width/height
-;            NewHeight := Height / r1
-;        else
-;            NewWidth := Width / r2
-;    }
-;    pBitmap := Gdip_CreateBitmap(NewWidth, NewHeight                        ; Create a new bitmap
-;    , (SubStr(NewExt, -2) = "bmp" && BitDepth = 24) ? 0x21808 : 0x26200A)   ; .bmp files use a bit depth of 24 by default
-;    G := Gdip_GraphicsFromImage(pBitmap)                                    ; Get a pointer to the graphics of the bitmap
-;    Gdip_SetSmoothingMode(G, 4)                                             ; Quality settings
-;    Gdip_SetInterpolationMode(G, 7)
-;    Gdip_DrawImage(G, pBitmapFile, 0, 0, NewWidth, NewHeight)               ; Draw the original image onto the new bitmap
-;    Gdip_DisposeImage(pBitmapFile)                                          ; Delete the bitmap of the original image
-;    Gdip_SaveBitmapToFile(pBitmap, NewPath)                                 ; Save the new bitmap to file
-;    Gdip_DisposeImage(pBitmap)                                              ; Delete the new bitmap
-;    Gdip_DeleteGraphics(G)                                                  ; The graphics may now be deleted
-;}
--- mw2.rb
+++ mw2.rb
@@ -4,146 +4,55 @@ require 'terminal-table'
 require 'colorize'
 
 TIMES = {seconds: 1000.0, minutes: 60.0, hours: 60.0, days: 24.0, months: (365.25 / 12), years: 12.0}
-# JSON_PATH = 'weapons.json'
-JSON_PATH = 'weapons_small.json'
+MASTERY = {gold: 100, plat: 200, poly: 300, orion: 400}
+THREADS = 16
+JSON_PATH = 'weapons.json'
+# JSON_PATH = 'weapons_small.json'
 TESSDATA_PATH = './tessdata/'
-
-class WeaponClass
-  def initialize(name, gun_names)
-    @name = name
-    @guns = {}
-    gun_names.each do |gun_name|
-      @guns[gun_name] = Gun.new(gun_name)
-    end
-  end
-
-  def [](name)
-    @guns[name]
-  end
-end
-
-class Gun
-  def initialize(name)
-    @name = name
-    @masteries = {}
-  end
-
-  def [](name)
-    @masteries[name]
-  end
-
-  def []=(name, value)
-    if value.empty?
-      @masteries[name] = :complete
-    else
-      @masteries[name] = value.to_i
-    end
-  end
-end
 
 def run_ahk
   `/mnt/c/Program\\ Files/AutoHotkey/v2/AutoHotkey.exe gather_screenshots.ahk`
 end
 
-def load_json(path)
-  JSON.load_file(path).map do |klass, guns|
-    [klass, WeaponClass.new(klass, guns)]
-  end.to_h
-end
-
 def main
-  # gun_map = load_json(JSON_PATH)
   gun_map = Hash.new { |hsh, key| hsh[key] = Hash.new {|h, k| h[k] = {}}}
 
-  pool = Concurrent::FixedThreadPool.new(16)
-  # pool = Concurrent::FixedThreadPool.new(1)
-  # Dir.glob('./runs/flat/*.png', File::FNM_DOTMATCH).each do |img|
-  #   pool.post do
-  #     # `convert '#{img}' -monochrome -channel RGB -negate '#{img}'`
-  #
-  #     # puts "tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/\\|\\\\ '#{img}' stdout"
-  #     progress = `tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/\\|\\\\ '#{img}' stdout`.strip
-  #     # puts "#{File.basename(img)}: #{progress} (#{progress.empty?})"# if img.include?('flat')
-  #
-  #     gun_class_name = File.basename(File.dirname(File.dirname(img)))
-  #     gun_name = File.basename(File.dirname(img))
-  #     mastery = File.basename(img, ".png").to_sym
-  #     puts "#{gun_class_name}: #{gun_name}: #{mastery}: #{progress}"
-  #     # gun_map[gun_class_name][gun_name][mastery] = calc_progress(progress)
-  #   end
-  # end
+  pool = Concurrent::FixedThreadPool.new(THREADS)
 
-  Dir.glob('./runs/**/*.png', File::FNM_DOTMATCH).reject do |img|
-  # Dir["./runs/**/*.png"].reject do |img|
+  gun_images = Dir.glob('./runs/**/*.png', File::FNM_DOTMATCH).reject do |img|
     img.end_with?('full.png') || img.end_with?('test.png') || img.include?('/flat/')
-  end.to_a.each do |img|
+  end
+  done_count = 0
+  gun_images.each do |img|
     pool.post do
-      # puts "img: #{img}"
-      # width = `identify -format "%w" '#{img}'`.to_i
-      # puts "width: #{width}"
-      # new_img = img[0..-5] + "_test.png"
-      # puts "cmd: #{"convert '#{img}' -resize #{width * 5} '#{new_img}'"}"
-      # `convert '#{img}' -resize #{width * 5} '#{img}'`
-      # `convert '#{img}' -resize #{width * 5} -bordercolor White -border 10x10 '#{img}'`
-      # `convert '#{img}' -scale 500% -bordercolor White -border 10x10 '#{img}'`
-      # `convert '#{img}' -magnify -magnify -magnify -bordercolor White -border 10x10 '#{img}'`
-      # `convert '#{img}' -adaptive-resize 500% -bordercolor White -border 10x10 '#{img}'`
-      # `convert '#{img}' -scale 500% -resample 600 -bordercolor White -border 10x10 '#{img}'`
-      # `convert '#{img}' -density 600 -bordercolor White -border 10x10 '#{img}'`
-      # `convert '#{img}' -threshold 75% '#{img}'`
-      # `convert '#{img}' -monochrome '#{img}'`
-
-      # puts "convert '#{img}' -monochrome -channel RGB -negate '#{img}'" if img.include? "Handgun"
       `convert '#{img}' -monochrome -channel RGB -negate '#{img}'`
-      # `convert '#{img}' -scale 500% -monochrome -channel RGB -negate '#{img}'`
-      # `convert '#{img}' -magnify -magnify -magnify -monochrome -channel RGB -negate '#{img}'`
-      # `convert '#{img}' -magnify -magnify -magnify -channel RGB -negate -monochrome '#{img}'`
-      # `convert '#{img}' -monochrome -magnify -magnify -magnify -channel RGB -negate '#{img}'`
-
-      # `convert '#{img}' -monochrome -channel RGB -negate -bordercolor White -border 10x10 '#{img}'`
-      # `convert '#{img}' -monochrome -channel RGB -negate -gaussian-blur 1x1 '#{img}'`
-      # `convert '#{img}' -monochrome -channel RGB -negate -gaussian-blur 0x1 '#{img}'`
-      # `convert '#{img}' -monochrome -channel RGB -negate -gaussian-blur 0x2 '#{img}'`
-
-      # `convert '#{img}' -monochrome -channel RGB -negate -morphology Smooth Octagon:1 '#{img}'`
-
-
-      # progress = `tesseract '#{img}' stdout`.strip
-      # progress = `tesseract -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`.strip
-      # progress = `tesseract -c load_number_dawg=1 '#{img}' stdout`.strip
-      # progress = `tesseract --psm 13 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`.strip
-      # progress = `tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`.strip
-      # progress = `tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/\\|\\\\l '#{img}' stdout`.strip
-      # progress = `tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/\\|\\\\l -c hocr_char_boxes=1 '#{img}' stdout tsv`.strip
-      # progress = `tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/\\|\\\\lI -c hocr_char_boxes=1 '#{img}' stdout hcr`.strip
-      # puts progress
-
-      # progress = `tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`.strip
-      # progress = `tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/\\|\\\\ '#{img}' stdout`.strip
-      # progress = `tesseract --psm 6 -c load_system_dawg=0 -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`
-      # progress = `tesseract -c load_system_dawg=0 -c load_freq_dawg=0 -c load_unambig_dawg=0 -c load_bigram_dawg=0 -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`
-      # puts "#{File.basename(img)}: #{progress} (#{progress.empty?})"# if img.include?('flat')
-
       progress = `TESSDATA_PREFIX=#{TESSDATA_PATH} tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`.strip
-      # progress = `TESSDATA_PREFIX=~/tessdata/ tesseract --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`.strip
-      # progress = `TESSDATA_PREFIX=~/tessdata/ tesseract --oem 0 --psm 6 -c load_number_dawg=1 -c tessedit_char_whitelist=0123456789/ '#{img}' stdout`.strip
 
       gun_class_name = File.basename(File.dirname(File.dirname(img)))
       gun_name = File.basename(File.dirname(img))
       mastery = File.basename(img, ".png").to_sym
-      # puts "#{gun_class_name}: #{gun_name}: #{mastery}: #{progress}"
       gun_map[gun_class_name][gun_name][mastery] = calc_progress(progress)
+
+      done_count += 1
+      print("\rAnalysing gun #{done_count / 4} / #{gun_images.size / 4}")
     end
   end
 
   pool.shutdown
   pool.wait_for_termination
+  puts ""
 
   print_gun_map(gun_map)
 end
 
 def calc_progress(str)
   str.empty? ? :complete : str.to_i
+end
+
+def load_json(path)
+  JSON.load_file(path).map do |klass, guns|
+    [klass, WeaponClass.new(klass, guns)]
+  end.to_h
 end
 
 def print_gun_map(gun_map)
@@ -178,8 +87,6 @@ def print_gun_map(gun_map)
   table = Terminal::Table.new(rows: rows, headings: ['Name', 'Gold', 'Platinum', 'Polyatomic', 'Orion'])
   puts table
 end
-
-MASTERY = {gold: 100, plat: 200, poly: 300, orion: 400}
 
 def print_row(masteries, mastery, weapon_class_totals)
   if masteries[mastery] == :complete
